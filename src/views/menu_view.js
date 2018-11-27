5996fd79b69738f0c9499671d3ced276f88b1336
Paul Wishart <misterwishart@gmail.com>
2018-11-27 15:16:28 +0000

    Added some lovely CSS Extension

--- src/views/menu_view.js
+++ src/views/menu_view.js
@@ -5,10 +5,14 @@ const MenuView = function () {
 };
 
 MenuView.prototype.bindEvents = function () {
-  console.log("hello, this is working");
+
 
   const menu = document.querySelector(".planets-menu");
   menu.addEventListener('click', (event) => {
+    if (event.target === menu) {
+      return;
+    }
+
     PubSub.publish('Planet:Selected-Planet', event.target.id);
   });
 };
